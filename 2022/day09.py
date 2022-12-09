7f95f4f36791723117fe2df7be3952690a29452a
Michael Bell <bellmr@gmail.com>
2022-12-09 06:28:35 -0500

    2922 day 9 part 2

--- 2022/day09.py
+++ 2022/day09.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Callable
 import helper
 
 
@@ -13,12 +13,12 @@ def clip(pos: Tuple[int, int], thresh: int=1) -> Tuple[int, int]:
 
 
 class Rope(object):
-    def __init__(self):
+    def __init__(self, n_knots: int=2):
         self.head_position = (0, 0)
-        self.tail_position = (0, 0)
-        self.tail_positions = [self.tail_position]
+        self.tail_positions = [(0, 0) for _ in range(n_knots - 1)]
+        self.tail_positions_visited = [self.tail_positions[-1]]
     
-    def move(self, command: str):
+    def move(self, command: str, print_func: Callable=print, verbose: bool=False):
         direction, distance = command[0], int(command[1:])
         for _ in range(distance): 
             if direction == 'R':
@@ -32,14 +32,26 @@ class Rope(object):
             else:
                 raise ValueError(f'Unknown direction: {direction}')
             self.move_tail()
-            # print(f'Head: {self.head_position}, Tail: {self.tail_position}')
+            if verbose:
+                print_func(self)
 
     def move_tail(self):
-        head_tail_offset = (self.head_position[0] - self.tail_position[0], self.head_position[1] - self.tail_position[1])
-        if abs(head_tail_offset[0]) > 1 or abs(head_tail_offset[1]) > 1:
-            head_tail_offset = clip(head_tail_offset)
-            self.tail_position = (self.tail_position[0] + head_tail_offset[0], self.tail_position[1] + head_tail_offset[1])
-            self.tail_positions.append(self.tail_position)
+
+        for i, tail in enumerate(self.tail_positions):
+            if i == 0:
+                lead = self.head_position
+            else:
+                lead = self.tail_positions[i - 1]
+
+            head_tail_offset = (lead[0] - tail[0], lead[1] - tail[1])
+            if abs(head_tail_offset[0]) > 1 or abs(head_tail_offset[1]) > 1:
+                head_tail_offset = clip(head_tail_offset)
+                self.tail_positions[i] = (
+                    self.tail_positions[i][0] + head_tail_offset[0], 
+                    self.tail_positions[i][1] + head_tail_offset[1]
+                )
+
+        self.tail_positions_visited.append(self.tail_positions[-1])
         
 
 if __name__ == '__main__':
@@ -55,12 +67,20 @@ R 2'''.split('\n')
     rope = Rope()
     for move in test_moves:
         rope.move(move)
-    assert len(set(rope.tail_positions)) == 13
+    assert len(set(rope.tail_positions_visited)) == 13
+
+    rope = Rope(n_knots=10)
+    for move in test_moves:
+        rope.move(move)
+    assert len(set(rope.tail_positions_visited)) == 1
 
     ### THE REAL THING
     puzzle_input = helper.read_input_lines()
     rope = Rope()
     for move in puzzle_input:
         rope.move(move)
-    print(f'Part 1: {len(set(rope.tail_positions))}')
-    print(f'Part 2: {""}')
+    print(f'Part 1: {len(set(rope.tail_positions_visited))}')
+    rope = Rope(n_knots=10)
+    for move in puzzle_input:
+        rope.move(move)
+    print(f'Part 2: {len(set(rope.tail_positions_visited))}')
