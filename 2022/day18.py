9dfd4fb9b22d58e02266b8f0b62ad87da7a2b95a
Michael Bell <bellmr@gmail.com>
2022-12-24 07:14:33 -0500

    2022 Day 18 part 2 solution that I think works, but takes too long so I haven't gotten part 2 yet. Works on the example though.

--- 2022/day18.py
+++ 2022/day18.py
@@ -1,25 +1,88 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import helper
+
+
+# For part 2, here is what I'm thinking
+# An exterior surface is one for which you can find a path to the outside by moving to adjacent coordinates
+# Find the extreme coordinates in each direction... once you are able to move beyond those bounds you're "outside"
+# For each cube, walk out from each side until you hit the outside or a dead end
+# This solution might be slow, but it should work
+
+
+Cube = Tuple[int, int, int]
+directions = [(0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)]
 
 
 class Droplet(object):
     def __init__(self, cube_coords: List[str]):
-        self.cubes: List[Tuple[int, int, int]] = []
+        self.cubes: List['Cube'] = []
         self._surface_area = 0
         for cube_coord in cube_coords:
             x, y, z = cube_coord.split(',')
             self.cubes.append((int(x), int(y), int(z)))
-
+        self._bounds = None
 
     @property 
     def surface_area(self) -> int:
         if self._surface_area == 0:
             for cube in self.cubes:
-                for side in [(0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)]:
+                for side in directions:
                     if (cube[0] + side[0], cube[1] + side[1], cube[2] + side[2]) not in self.cubes:
                         self._surface_area += 1
         return self._surface_area
+
+    @property
+    def bounds(self) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+        if self._bounds is None:
+            x_min = min([cube[0] for cube in self.cubes])
+            x_max = max([cube[0] for cube in self.cubes])
+            y_min = min([cube[1] for cube in self.cubes])
+            y_max = max([cube[1] for cube in self.cubes])
+            z_min = min([cube[2] for cube in self.cubes])
+            z_max = max([cube[2] for cube in self.cubes])
+
+            self._bounds = ((x_min, x_max), (y_min, y_max), (z_min, z_max))
+        return self._bounds
+
+    def oob(self, cube: 'Cube') -> bool:
+        x, y, z = cube
+        x_min, x_max = self.bounds[0]
+        y_min, y_max = self.bounds[1]
+        z_min, z_max = self.bounds[2]
+        return x < x_min or x > x_max or y < y_min or y > y_max or z < z_min or z > z_max
+
+
+def seek_exterior(droplet: 'Droplet', coord: 'Cube', visited_coords: Optional[List['Cube']]=None) -> bool:
+    if visited_coords is None:
+        visited_coords = []
+
+    if droplet.oob(coord):
+        return True
+    if coord in droplet.cubes:
+        return False
+    for side in directions:
+        next_coord = (coord[0] + side[0], coord[1] + side[1], coord[2] + side[2])
+
+        if (
+            (next_coord not in visited_coords) and 
+            seek_exterior(droplet, next_coord, visited_coords + [coord])
+        ):
+            return True
+    return False
+
+
+def exterior_surface_area(droplet: 'Droplet') -> int:
     
+    surface_area = 0
+
+    for cube in droplet.cubes:
+        for side in directions:
+            current_position = (cube[0] + side[0], cube[1] + side[1], cube[2] + side[2]) 
+            if seek_exterior(droplet, current_position):
+                surface_area += 1
+    
+    return surface_area
+
 
 if __name__ == '__main__':
     ### THE TESTS
@@ -39,9 +102,11 @@ if __name__ == '__main__':
 
     test_droplet = Droplet(test_input)
     assert test_droplet.surface_area == 64
+    assert exterior_surface_area(test_droplet) == 58
 
     ### THE REAL THING
     puzzle_input = helper.read_input_lines()
     droplet = Droplet(puzzle_input)
     print(f'Part 1: {droplet.surface_area}')
-    print(f'Part 2: {""}')
+    surface_area = exterior_surface_area(droplet)
+    print(f'Part 2: {surface_area}')
