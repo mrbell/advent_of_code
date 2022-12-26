59016b8bba920ad86c56eee6d0afc4f289466aaf
Michael Bell <bellmr@gmail.com>
2022-12-26 08:35:17 -0500

    2022 Day 18, added more tests and finally found one that fails. I know why. Will attempt a fix.

--- 2022/day18.py
+++ 2022/day18.py
@@ -3,10 +3,12 @@ import helper
 
 
 # For part 2, here is what I'm thinking
-# An exterior surface is one for which you can find a path to the outside by moving to adjacent coordinates
-# Find the extreme coordinates in each direction... once you are able to move beyond those bounds you're "outside"
-# For each cube, walk out from each side until you hit the outside or a dead end
-# This solution might be slow, but it should work
+# An exterior surface is one for which you can find a path to the outside by moving between 
+# adjacent "air" cubes (those not in the droplet, can't move diagonally)
+# Find the extreme coordinates in each direction... once you are able to move beyond those 
+# bounds you're "outside"
+# For each cube in the droplet, search out from each side until you hit the outside or a dead end
+# Count each side that leads to the outside as an exterior surface
 
 
 Cube = Tuple[int, int, int]
@@ -34,12 +36,13 @@ class Droplet(object):
     @property
     def bounds(self) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
         if self._bounds is None:
-            x_min = min([cube[0] for cube in self.cubes])
-            x_max = max([cube[0] for cube in self.cubes])
-            y_min = min([cube[1] for cube in self.cubes])
-            y_max = max([cube[1] for cube in self.cubes])
-            z_min = min([cube[2] for cube in self.cubes])
-            z_max = max([cube[2] for cube in self.cubes])
+            padding = 0
+            x_min = min([cube[0] for cube in self.cubes]) - padding
+            x_max = max([cube[0] for cube in self.cubes]) + padding
+            y_min = min([cube[1] for cube in self.cubes]) - padding
+            y_max = max([cube[1] for cube in self.cubes]) + padding
+            z_min = min([cube[2] for cube in self.cubes]) - padding
+            z_max = max([cube[2] for cube in self.cubes]) + padding
 
             self._bounds = ((x_min, x_max), (y_min, y_max), (z_min, z_max))
         return self._bounds
@@ -66,6 +69,11 @@ def seek_exterior(
         return True
     if coord in droplet.cubes:
         return False
+    if coord in leads_out:
+        return True
+    if coord in leads_to_dead_end:
+        return False
+
     for side in directions:
         next_coord = (coord[0] + side[0], coord[1] + side[1], coord[2] + side[2])
         if next_coord in visited_coords:
@@ -100,6 +108,7 @@ def exterior_surface_area(droplet: 'Droplet') -> int:
 
 if __name__ == '__main__':
     ### THE TESTS
+    # Made some test cases with https://voxelator.com/#
     test_input = '''2,2,2
 1,2,2
 3,2,2
@@ -112,11 +121,440 @@ if __name__ == '__main__':
 1,2,5
 3,2,5
 2,1,5
-2,3,5'''.split('\n')
+2,3,5'''.split('\n')  # 64 total, 58 exterior
 
     test_droplet = Droplet(test_input)
     assert test_droplet.surface_area == 64
-    assert exterior_surface_area(test_droplet) == 58
+    assert exterior_surface_area(test_droplet) == 58 
+
+    test_input += [
+        '2,3,6',
+        '3,3,5',
+        '3,3,6',
+        '2,2,7',
+        '4,2,5',
+        '3,2,7',
+        '4,2,6',
+        '3,1,5',
+        '2,1,6',
+        '3,1,6',
+        '2,4,2',
+        '3,4,2',
+        '2,3,3',
+        '3,3,3'
+    ]  # 106 total, 94 exterior
+
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 106 
+    assert exterior_surface_area(test_droplet) == 94 
+
+    test_input = '''0,0,0
+1,0,0
+2,0,0
+3,0,0
+4,0,0
+0,1,0
+1,1,0
+2,1,0
+3,1,0
+4,1,0
+0,2,0
+1,2,0
+2,2,0
+3,2,0
+4,2,0
+0,0,1
+0,1,1
+0,2,1
+1,0,1
+0,0,2
+1,0,2
+0,1,2
+0,2,2
+1,2,1
+1,2,2
+1,1,2
+4,0,1
+4,0,2
+4,1,1
+4,2,1
+4,1,2
+4,2,2
+3,0,1
+3,0,2
+3,2,1
+3,2,2
+3,1,2
+2,0,1
+2,2,1'''.split('\n')  # 94 total and exterior
+
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 94 
+    assert exterior_surface_area(test_droplet) == 94 
+
+    test_input += ['2,1,2']  # 96 total, 82 exterior
+
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 96 
+    assert exterior_surface_area(test_droplet) == 82 
+
+    test_input = '''1,1,1
+2,1,1
+3,1,1
+4,1,1
+5,1,1
+6,1,1
+1,2,1
+2,2,1
+3,2,1
+4,2,1
+5,2,1
+6,2,1
+1,3,1
+2,3,1
+3,3,1
+4,3,1
+5,3,1
+6,3,1
+1,1,2
+2,1,2
+3,1,2
+4,1,2
+5,1,2
+6,1,2
+1,2,2
+6,2,2
+1,3,2
+2,3,2
+3,3,2
+4,3,2
+5,3,2
+6,3,2
+1,1,3
+2,1,3
+3,1,3
+4,1,3
+5,1,3
+6,1,3
+1,2,3
+2,2,3
+3,2,3
+4,2,3
+5,2,3
+6,2,3
+1,3,3
+2,3,3
+3,3,3
+4,3,3
+5,3,3
+6,3,3'''.split('\n')  # 108 total, 90 exterior
+
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 108 
+    assert exterior_surface_area(test_droplet) == 90 
+
+    test_input = '''0,0,0
+1,0,0
+2,0,0
+3,0,0
+4,0,0
+0,1,0
+1,1,0
+2,1,0
+3,1,0
+4,1,0
+0,2,0
+1,2,0
+2,2,0
+3,2,0
+4,2,0
+0,3,0
+1,3,0
+2,3,0
+3,3,0
+4,3,0
+0,4,0
+1,4,0
+2,4,0
+3,4,0
+4,4,0
+0,0,1
+0,0,2
+0,0,3
+0,0,4
+0,1,1
+0,1,2
+0,1,3
+0,1,4
+0,2,1
+0,2,2
+0,2,3
+0,2,4
+0,3,1
+0,3,2
+0,3,3
+0,3,4
+0,4,1
+0,4,2
+0,4,3
+0,4,4
+1,0,1
+1,0,2
+1,0,3
+1,0,4
+2,0,1
+2,0,2
+2,0,3
+2,0,4
+3,0,1
+3,0,2
+3,0,3
+3,0,4
+4,0,1
+4,0,2
+4,0,3
+4,0,4
+1,4,1
+1,4,2
+1,4,3
+1,4,4
+2,4,1
+2,4,2
+2,4,3
+2,4,4
+3,4,1
+3,4,2
+3,4,3
+3,4,4
+4,4,1
+4,4,2
+4,4,3
+4,4,4
+1,3,4
+1,2,4
+1,1,4
+2,3,4
+2,2,4
+2,1,4
+3,3,4
+3,2,4
+3,1,4
+4,3,4
+4,2,4
+4,1,4
+4,3,3
+4,2,3
+4,1,3
+4,3,2
+4,2,2
+4,1,2
+4,3,1
+4,2,1
+4,1,1'''.split('\n')  # 204 total, 150 exterior
+    
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 204 
+    assert exterior_surface_area(test_droplet) == 150 
+
+    test_input += '''0,5,0
+0,5,-1
+-1,5,0
+-1,5,-1
+0,5,-2
+-1,5,-2
+-2,5,-1
+-2,5,0
+-1,4,0
+0,4,-1
+0,4,-2
+-2,4,0
+0,3,-1
+-1,3,0
+-1,3,-1
+-1,4,-2
+-2,4,-1'''.split('\n')  # 248 total, 188 exterior
+    
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 248 
+    assert exterior_surface_area(test_droplet) == 188 
+
+    test2 = '''1,1,1
+2,1,1
+3,1,1
+4,1,1
+5,1,1
+6,1,1
+1,2,1
+2,2,1
+4,2,1
+5,2,1
+6,2,1
+1,3,1
+2,3,1
+3,3,1
+4,3,1
+5,3,1
+6,3,1
+1,4,1
+2,4,1
+3,4,1
+4,4,1
+5,4,1
+6,4,1
+1,1,2
+2,1,2
+3,1,2
+4,1,2
+5,1,2
+6,1,2
+1,2,2
+6,2,2
+1,3,2
+6,3,2
+1,4,2
+2,4,2
+3,4,2
+4,4,2
+5,4,2
+6,4,2
+1,1,3
+2,1,3
+3,1,3
+4,1,3
+5,1,3
+6,1,3
+1,2,3
+2,2,3
+3,2,3
+4,2,3
+5,2,3
+6,2,3
+1,3,3
+2,3,3
+3,3,3
+4,3,3
+5,3,3
+6,3,3
+1,4,3
+2,4,3
+3,4,3
+4,4,3
+5,4,3
+6,4,3
+1,1,4
+2,1,4
+3,1,4
+4,1,4
+5,1,4
+6,1,4
+1,2,4
+6,2,4
+1,3,4
+6,3,4
+1,4,4
+2,4,4
+3,4,4
+4,4,4
+5,4,4
+6,4,4
+1,1,5
+2,1,5
+3,1,5
+4,1,5
+5,1,5
+6,1,5
+1,2,5
+2,2,5
+3,2,5
+4,2,5
+5,2,5
+6,2,5
+1,3,5
+2,3,5
+3,3,5
+4,3,5
+5,3,5
+6,3,5
+1,4,5
+2,4,5
+3,4,5
+4,4,5
+5,4,5
+6,4,5'''.split('\n')  # 178 exterior
+
+    test_droplet = Droplet(test2)
+    assert exterior_surface_area(test_droplet) == 178 
+
+    test_input = '''1,1,1
+2,1,1
+3,1,1
+4,1,1
+5,1,1
+6,1,1
+1,2,1
+2,2,1
+3,2,1
+4,2,1
+5,2,1
+6,2,1
+1,3,1
+2,3,1
+3,3,1
+4,3,1
+5,3,1
+6,3,1
+1,1,2
+2,1,2
+3,1,2
+4,1,2
+5,1,2
+6,1,2
+1,2,2
+6,2,2
+1,3,2
+2,3,2
+3,3,2
+4,3,2
+5,3,2
+6,3,2
+1,1,3
+2,1,3
+3,1,3
+4,1,3
+5,1,3
+6,1,3
+1,2,3
+2,2,3
+3,2,3
+4,2,3
+5,2,3
+6,2,3
+1,3,3
+2,3,3
+3,3,3
+4,3,3
+5,3,3
+6,3,3'''.split('\n')  # 86, 80
+    
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 108 
+    assert exterior_surface_area(test_droplet) == 90 
+
+    test_input = '''2,2,3
+2,4,3
+2,3,2
+2,3,4
+3,2,3
+3,4,3
+3,3,2
+3,3,4
+4,3,3'''.split('\n')  # 46 for both
+
+    # This test is failing!
+    test_droplet = Droplet(test_input)
+    assert test_droplet.surface_area == 46
+    assert exterior_surface_area(test_droplet) == 46
+
 
     ### THE REAL THING
     puzzle_input = helper.read_input_lines()
