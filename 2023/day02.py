d0cd4e735e4968d95008327b3f26758773a204f6
Michael Bell <bellmr@gmail.com>
2023-12-02 08:22:59 -0500

    day 2 part 2

--- 2023/day02.py
+++ 2023/day02.py
@@ -21,6 +21,20 @@ def parse_line(s: str) -> Tuple[int, List[Dict[str, int]]]:
 
     return game_id, cube_sets
 
+def min_cubes(cube_sets: List[Dict[str, int]]) -> Dict[str, int]:
+    min_cube_dict = {color: -1 for color in colors}
+    for cube_set in cube_sets:
+        for color in colors:
+            min_cube_dict[color] = max(min_cube_dict[color], cube_set[color])
+    return min_cube_dict
+
+
+def set_power(cube_set: Dict[str, int]) -> int:
+    product = 1
+    for color in colors:
+        product *= cube_set[color]
+    return product
+
 
 def check_if_possible(cube_sets: List[Dict[str, int]], cube_limits: Dict[str, int]) -> bool:
 
@@ -37,6 +51,12 @@ def part1(puzzle_input: List[str]) -> int:
     return game_sum
 
 
+def part2(puzzle_input: List[str]) -> int:
+    games = [parse_line(i) for i in puzzle_input]
+    power_sum = sum(set_power(min_cubes(game[1])) for game in games)
+    return power_sum
+
+
 if __name__ == '__main__':
     ### THE TESTS
 
@@ -47,8 +67,9 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''.split('\n')
 
     assert part1(test_input) == 8
+    assert part2(test_input) == 2286
 
     ### THE REAL THING
     puzzle_input = helper.read_input_lines()
     print(f'Part 1: {part1(puzzle_input)}')
-    print(f'Part 2: {""}')
+    print(f'Part 2: {part2(puzzle_input)}')
