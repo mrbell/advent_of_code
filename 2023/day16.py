43d5abb278d710ddcdab1fe88a7ec6fa07387466
Michael Bell <bellmr@gmail.com>
2023-12-22 16:06:29 -0500

    day 16 part 2

--- 2023/day16.py
+++ 2023/day16.py
@@ -74,8 +74,16 @@ class Beam(object):
         return new_beams
 
 
-def part1(contraption: List[str]) -> int:
-    beams = [Beam((0, 0), 'right')]
+def part1(
+        contraption: List[str], 
+        start_pos: Optional[Tuple[int, int]]=None, 
+        start_dir: Optional[str]=None
+) -> int:
+    if start_pos is None:
+        start_pos = (0, 0)
+    if start_dir is None:
+        start_dir = 'right'
+    beams = [Beam(start_pos, start_dir)]
     followed_beams = []
     while len(beams) > 0:
         beam = beams.pop(0)     
@@ -93,6 +101,22 @@ def part1(contraption: List[str]) -> int:
     return len(set(energized_positions))
 
 
+def part2(contraption: List[str]) -> int:
+    
+    results = {}
+
+    for col, dir in zip([0, len(contraption[0])-1], ['right', 'left']):
+        for row in range(len(contraption)):
+            result = part1(contraption, (row, col), dir)
+            results[(row, col, dir)] = result
+
+    for row, dir in zip([0, len(contraption)-1], ['down', 'up']):
+        for col in range(len(contraption[0])):
+            result = part1(contraption, (row, col), dir)
+            results[(row, col, dir)] = result
+    
+    return max(results.values())
+
 
 if __name__ == '__main__':
     ### THE TESTS
@@ -109,8 +133,10 @@ if __name__ == '__main__':
     result = part1(test_input)
     print(result)
     assert result == 46
+    assert part1(test_input, (0, 3), 'down') == 51
+    assert part2(test_input) == 51
 
     ### THE REAL THING
     puzzle_input = helper.read_input_lines()
     print(f'Part 1: {part1(puzzle_input)}')
-    print(f'Part 2: {""}')
+    print(f'Part 2: {part2(puzzle_input)}')
