6fb43ed9186e2f37062461b3cc0bc7de045fc48b
Michael Bell <michael@titanaes.com>
2024-12-22 17:46:37 -0500

    Day 19 2024

--- 2024/day19.py
+++ 2024/day19.py
@@ -2,12 +2,93 @@ from typing import List, Tuple
 import helper
 
 
+def parse_input(input_string: str) -> Tuple[List[str], List[str]]:
+    towels, arrangements = input_string.split('\n\n')
+    return towels.split(', '), arrangements.split('\n')
+
+
+def is_possible(towels: List[str], arrangement: str) -> bool:
+    
+    it_is_possible = False
+
+    for towel in towels:
+        if arrangement == towel:
+            return True
+        if arrangement.find(towel) == 0:
+            if is_possible(towels, arrangement[len(towel):]):
+                return True
+    return it_is_possible
+
+
+class ArrangementFinder(object):
+
+    def __init__(self, towels: List[str]):
+        self.arrangement_cache = {}
+        self.towels = towels
+
+
+    def get_possible_arrangements(self, arrangement: str) -> int:
+
+        if arrangement in self.arrangement_cache:
+            return self.arrangement_cache[arrangement]
+
+        possible_arrangements = 0
+        for towel in self.towels:
+            if arrangement == towel:
+                possible_arrangements += 1
+            if arrangement.find(towel) == 0:
+                possible_arrangements += self.get_possible_arrangements(arrangement[len(towel):])
+
+        self.arrangement_cache[arrangement] = possible_arrangements
+
+        return possible_arrangements
+
+
+def count_possible(towels: List[str], arrangements: List[str]) -> int:
+    # print(f'Towels: {towels}')
+    arrangement_is_possible = [is_possible(towels, arrangement) for arrangement in arrangements]
+    # for arrangement, arrangement_possible in zip(arrangements, arrangement_is_possible):
+    #     print(f'{arrangement}: {arrangement_possible}')
+
+    return sum(arrangement_is_possible)
+
+
+def part1(puzzle_input: str) -> int:
+    towels, arrangements = parse_input(puzzle_input)
+    return count_possible(towels, arrangements)
+
+
+def part2(puzzle_input: str) -> int:
+    towels, arrangements = parse_input(puzzle_input)
+    arrangement_finder = ArrangementFinder(towels)
+    total_arrangements = 0
+    for arrangement in arrangements:
+        possible_arrangements = arrangement_finder.get_possible_arrangements(arrangement)
+        print(f'{arrangement}: {possible_arrangements}')
+        total_arrangements += possible_arrangements
+    return total_arrangements
+
 
 if __name__ == '__main__':
     ### THE TESTS
+    example_input = '''r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb'''
+    result = part1(example_input)
+    helper.check(result, 6)
+    result = part2(example_input)
+    helper.check(result, 16)
+
 
 
     ### THE REAL THING
     puzzle_input = helper.read_input()
-    print(f'Part 1: {""}')
-    print(f'Part 2: {""}')
+    print(f'Part 1: {part1(puzzle_input)}')
+    print(f'Part 2: {part2(puzzle_input)}')
