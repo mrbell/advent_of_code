4a3b83d24f81617d6ce552fa3e786e1a4e5d2b5b
Michael Bell <michael@titanaes.com>
2024-12-22 11:09:47 -0500

    Day 17 part 2 kind of manually searched.

--- 2024/day17.py
+++ 2024/day17.py
@@ -11,6 +11,12 @@ class ThreeBitComputer(object):
         self.registers = registers
         self.pointer = 0
         self.output = []
+    
+    def print_program(self):
+        print(','.join(list(map(str, self.program))))
+    
+    def print_output(self):
+        print(','.join(list(map(str, self.output))))
 
     def combo(self, operand):
         if operand <= 3: 
@@ -101,16 +107,140 @@ def part1(puzzle_input: str) -> str:
     return computer.run()
 
 
+def find_output_lower(puzzle_input: str) -> int:
+    Areg = 1
+    Areg_lower = None
+    Areg_upper = None
+    while Areg_lower is None or Areg_upper is None:
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        if len(computer.output) < len(computer.program):
+            if Areg_lower is None:
+                Areg_lower = Areg
+            elif Areg > Areg_lower:
+                Areg_lower = Areg
+            Areg *= 2 ** (len(computer.program) - len(computer.output))
+        elif len(computer.output) >= len(computer.program):
+            if Areg_upper is None:
+                Areg_upper = Areg
+            elif Areg < Areg_upper:
+                Areg_upper = Areg
+            Areg = Areg // 2
+    
+    while True:
+        Areg = (Areg_lower + Areg_upper) // 2
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        if len(computer.output) < len(computer.program):
+            other_computer = parse(puzzle_input)
+            other_computer.registers['A'] = Areg + 1
+            other_result = other_computer.run()
+            if len(other_computer.output) == len(computer.program):
+                return Areg + 1
+            Areg_lower = Areg
+        elif len(computer.output) > len(computer.program):
+            Areg_upper = Areg
+        else:
+            other_computer = parse(puzzle_input)
+            other_computer.registers['A'] = Areg - 1
+            other_result = other_computer.run()
+            if len(other_computer.output) < len(computer.program):
+                return Areg
+            Areg_upper = Areg
+
+
+
+
+def find_output_upper(puzzle_input: str) -> int:
+    Areg = 1
+    Areg_lower = None
+    Areg_upper = None
+    while Areg_lower is None or Areg_upper is None:
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        if len(computer.output) <= len(computer.program):
+            if Areg_lower is None:
+                Areg_lower = Areg
+            elif Areg > Areg_lower:
+                Areg_lower = Areg
+            Areg *= 2 ** (len(computer.program) - len(computer.output) + 1)
+        elif len(computer.output) > len(computer.program):
+            if Areg_upper is None:
+                Areg_upper = Areg
+            elif Areg < Areg_upper:
+                Areg_upper = Areg
+            Areg = Areg // 2
+    
+    while True:
+        Areg = (Areg_lower + Areg_upper) // 2
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        if len(computer.output) < len(computer.program):
+            Areg_lower = Areg
+        elif len(computer.output) > len(computer.program):
+            Areg_upper = Areg
+        else:
+            other_computer = parse(puzzle_input)
+            other_computer.registers['A'] = Areg + 1
+            other_result = other_computer.run()
+            if len(other_computer.output) > len(computer.program):
+                return Areg
+            Areg_lower = Areg
+
+
+
 def part2(puzzle_input: str) -> int:
+
+    lower_Areg = find_output_lower(puzzle_input)
+    upper_Areg = find_output_upper(puzzle_input)
+    lower_Areg = 190384609150581 
+    upper_Areg = 190384613365317
+    print(f'Lower: {lower_Areg},  Upper: {upper_Areg}')
+    print(f'Range: {upper_Areg - lower_Areg}')
+    print('-----------------')
+
+    Aregs = range(lower_Areg, upper_Areg + 1)
+    for Areg in Aregs:
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        if computer.output == computer.program:
+            return Areg
+
+    Aregs = range(lower_Areg, upper_Areg + 1, (upper_Areg - lower_Areg) // 200)
+    for Areg in Aregs:
+        computer = parse(puzzle_input)
+        computer.registers['A'] = Areg
+        result = computer.run()
+        print(Areg, ':',result)
+
+    computer.print_program()
+
     Areg = 1
     while True:
         computer = parse(puzzle_input)
         computer.registers['A'] = Areg
-        if computer.run() == ','.join(map(str, computer.program)):
+        result = computer.run()
+        print(Areg, result)
+        if result == ','.join(map(str, computer.program)):
             return Areg
-        Areg += 1
-        if Areg % 1000 == 0:
-            print(Areg)
+        if len(computer.output) < len(computer.program):
+            Areg *= 2 ** (len(computer.program) - len(computer.output))
+        elif len(computer.output) > len(computer.program):
+            Areg = Areg // 2
+        else:
+            n_dig = 0
+            for i in range(len(computer.program), 0, -1):
+                if computer.program[i-1] == computer.output[i-1]:
+                    n_dig = i
+                else:
+                    break
+            n_dig = len(computer.program) - n_dig
+            Areg += 10 ** n_dig
 
 
 if __name__ == '__main__':
@@ -127,7 +257,7 @@ Register B: 0
 Register C: 0
 
 Program: 0,3,5,4,3,0'''
-    helper.check(part2(example_input), 117440)
+    # helper.check(part2(example_input), 117440)
 
 
     ### THE REAL THING
